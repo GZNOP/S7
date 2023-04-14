73c64aa071461b4bd16266b48dab9f469760c809
gznop <baillo83200@gmail.com>
2023-04-14 16:06:20 +0200

    i dirige

---
+++ I61/lib/entropie.py
@@ -0,0 +1,10 @@
+def probabilite(X, i):
+    """
+    X est la variable aléatoire composée des réalisations.
+    On calcule les probabilités de celles-ci
+    """
+    
+def entropie(X):
+    pass
+
+def
--- I63/lib/Segment.py
+++ I63/lib/Segment.py
@@ -1,4 +1,4 @@
-import lib.Point
+import lib.Point as p
 
 class Segment:
 
@@ -35,3 +35,39 @@ class Segment:
     @property
     def ep(self):
         return self._ep
+
+    def intersection(S1, S2):
+        """ Calcule le point d'intersection entre deux segments """
+
+        A = S1.P1
+        B = S1.P2
+        C = S2.P1
+        D = S2.P2
+
+        det = (B.x - A.x) * (C.y - D.y) - (C.x - D.x) * (B.y - A.y)
+        if det == 0:
+            return None # Segment parallèle
+
+        else:
+            t1 = ((C.x - A.x) * (C.y - D.y) - (C.x - D.x) * (C.y - A.y)) / det
+            t2 = ((B.x - A.x) * (C.y - A.y) - (C.x - A.x) * (B.y - A.y)) / det
+            
+            if 0 <= t1 <= 1 and 0 <= t2 <= 1:
+                
+                if t1 == 0:
+                    x,y = A.x,A.y
+                elif t1 == 1:
+                    x,y = B.x,B.y
+                elif t2 == 0:
+                    x,y = C.x,C.y
+                elif t2 == 1:
+                    x,y = D.x, D.y
+                else:
+                    x = A.x + t1 * (B.x - A.x)
+                    y = A.y + t1 * (B.y - A.y)
+
+                return p.Point2D(x,y)
+            
+            else:
+                # Pas d'intersection
+                return None
